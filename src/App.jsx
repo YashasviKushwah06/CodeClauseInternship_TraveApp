0b174576e42dee1990d60ce62e88d56db3644ddd
Yashasvi Kushwah <135824083+YashasviKushwah06@users.noreply.github.com>
2024-06-27 12:26:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './components/Navbar/navbar'
+import Home from './components/Home/home'
+import Body from './components/Body/body'
+import Footer from './components/Footer/footer'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Home />
+    <Body />
+    <Footer />      
+    </>
+  )
+}
+
+export default App
